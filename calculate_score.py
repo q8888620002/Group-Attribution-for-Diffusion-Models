fix id score calculation.

calculate_score.py
@@ -1,25 +1,14 @@
-import os
-import math
 import argparse
-import glob
-
 import torch
-import torch.nn as nn
 import numpy as np
 import torchvision
-from scipy.linalg import sqrtm
-from scipy import linalg
 
-from torchvision.utils import save_image
 from torch.utils.data import DataLoader
-from torch.optim import AdamW
-from torch.optim.lr_scheduler import OneCycleLR
-from torchvision.models import inception_v3
-from torchvision.transforms import Resize, Normalize, ToTensor, Compose, Lambda, Grayscale
-from scipy.linalg import sqrtm
+from torchvision import transforms
 
-
+from ddpm_config import DDPMConfig
 from diffusion.diffusions import DDPM
+from inception import InceptionV3
 from utils import *
 
 def parse_args():
@@ -44,170 +33,144 @@ def parse_args():
 
     return args
 
-def calculate_fid(real_features, fake_features):
-    # calculate mean and covariance statistics
-    mu1, sigma1 = real_features.mean(axis=0), np.cov(real_features, rowvar=False)
-    mu2, sigma2 = fake_features.mean(axis=0), np.cov(fake_features, rowvar=False)
-    
-    # calculate sum squared difference between means
-    ssdiff = np.sum((mu1 - mu2)**2.0)
-    
-    # calculate sqrt of product between cov
-    covmean = linalg.sqrtm(sigma1.dot(sigma2))
-    
-    # check and correct imaginary numbers from sqrt
-    if np.iscomplexobj(covmean):
-        covmean = covmean.real
-    
-    # calculate score
-    fid = ssdiff + np.trace(sigma1 + sigma2 - 2.0 * covmean)
-    return fid
-
-# Feature extraction
-def get_features(dataloader, model, n_samples):
-
-    
-    model.eval()
-
-    with torch.no_grad():
-
-        images, _ = next(iter(dataloader))
-        images = images.to(device)[:n_samples]
-        images = torch.stack([TF.resize(img, (299, 299), antialias=True) for img in images])
-
-        features = model(images)
-    return features.cpu().numpy()
 
 def main(args):
 
-    dataset_configs = {
-        "mnist": {
-            "timesteps":1000,
-            "base_dim": 64,
-            "image_size": 28,
-            "in_channels": 1,
-            "out_channels":1,
-            "channel_mult": [2, 4],
-        },
-        "cifar": {
-            "timesteps":1000,
-            "base_dim": 128,
-            "channel_mult": [1, 2, 3, 4],
-            "image_size": 32,
-            "in_channels": 3,
-            "out_channels": 3,
-        },
-    }
-
-
-
-    config = dataset_configs.get(args.dataset)
+    if args.dataset == "cifar":
+        config = {**DDPMConfig.cifar_config}
+    elif args.dataset  == "mnist":
+        config = {**DDPMConfig.mnist_config}
 
     if config is None:
         raise ValueError(f"Invalid dataset: {args.dataset}")
 
-    model_full = DDPM(**config).to(device)
-    model_ablated = DDPM(**config).to(device)
-    model_unlearn = DDPM(**config).to(device)
+    model_full =  DDPM(
+            timesteps=config['timesteps'],
+            base_dim=config['base_dim'],
+            channel_mult=config['channel_mult'],
+            image_size=config['image_size'],
+            in_channels=config['in_channels'],
+            out_channels=config['out_channels'],
+            attn=config['attn'],
+            attn_layer=config['attn_layer'],
+            num_res_blocks=config['num_res_blocks'],
+            dropout=config['dropout']
+        ).to(device).to(device)
+    
+    # model_ablated = DDPM(**config).to(device)
+    # model_unlearn = DDPM(**config).to(device)
 
-    #load checkpoint
-
-
-    def find_max_step_file(path_pattern):
-        files = glob.glob(path_pattern)
-        max_step_file = max(files, key=lambda x: int(os.path.basename(x).split('_')[1].split('.')[0]))
-        return max_step_file
 
     if args.dataset == "mnist":
+
+        ## TODO mnist evaluation
+
         max_step_file = find_max_step_file("/projects/leelab2/mingyulu/unlearning/results/full/models/steps_*.pt")
         ckpt = torch.load(max_step_file)
 
-        model_full.load_state_dict(ckpt["model"])
-        model_full.eval()
+        # model_full.load_state_dict(ckpt["model"])
+        # model_full.eval()
 
-        for i in range(1, 10):
-            max_step_file_ablated = find_max_step_file(f"/projects/leelab2/mingyulu/unlearning/results/ablated/{i}/models/steps_*.pt")
-            ckpt_ablated = torch.load(max_step_file_ablated)
-            model_ablated.load_state_dict(ckpt_ablated["model"])
-            model_ablated.eval()
+        # for i in range(1, 10):
+        #     max_step_file_ablated = find_max_step_file(f"/projects/leelab2/mingyulu/unlearning/results/ablated/{i}/models/steps_*.pt")
+        #     ckpt_ablated = torch.load(max_step_file_ablated)
+        #     model_ablated.load_state_dict(ckpt_ablated["model"])
+        #     model_ablated.eval()
 
-            max_step_file_unlearn = find_max_step_file(f"results/models/unlearn_remaining_ablated/{i}/epochs=100_datasets=False_loss=type1:alpha1=1.0_alpha2=0.01_weight_reg=False/steps_*.pt")
-            ckpt_unlearn = torch.load(max_step_file_unlearn)
-            model_unlearn.load_state_dict(ckpt_unlearn["model"])
-            model_unlearn.eval()
+        #     max_step_file_unlearn = find_max_step_file(f"results/models/unlearn_remaining_ablated/{i}/epochs=100_datasets=False_loss=type1:alpha1=1.0_alpha2=0.01_weight_reg=False/steps_*.pt")
+        #     ckpt_unlearn = torch.load(max_step_file_unlearn)
+        #     model_unlearn.load_state_dict(ckpt_unlearn["model"])
+        #     model_unlearn.eval()
 
-            x_t=torch.randn((args.n_samples, 1, config["image_size"], config["image_size"])).to(device)
+        #     x_t=torch.randn((args.n_samples, 1, config["image_size"], config["image_size"])).to(device)
 
-            samples_original = model_full._sampling(x_t, args.n_samples, clipped_reverse_diffusion=not args.no_clip, device=device)
-            samples_ablated = model_ablated._sampling(x_t, args.n_samples, clipped_reverse_diffusion=not args.no_clip, device=device)
-            samples_unlearn = model_unlearn._sampling(x_t, args.n_samples, clipped_reverse_diffusion=not args.no_clip, device=device)
+        #     samples_original = model_full._sampling(x_t, args.n_samples, clipped_reverse_diffusion=not args.no_clip, device=device)
+        #     samples_ablated = model_ablated._sampling(x_t, args.n_samples, clipped_reverse_diffusion=not args.no_clip, device=device)
+        #     samples_unlearn = model_unlearn._sampling(x_t, args.n_samples, clipped_reverse_diffusion=not args.no_clip, device=device)
 
 
-            print(clip_score(samples_unlearn, samples_original), clip_score(samples_ablated, samples_original), clip_score(samples_ablated, samples_unlearn))
+            # print(clip_score(samples_unlearn, samples_original), clip_score(samples_ablated, samples_original), clip_score(samples_ablated, samples_unlearn))
 
-    if args.dataset == "cifar":
+    elif args.dataset == "cifar":
 
         # max_step_file = find_max_step_file("results/cifar/retrain/models/full/steps_*.pt")
-        # import ipdb;ipdb.set_trace()
 
-        ckpt = torch.load("results/cifar/retrain/models/full/steps_00078200.pt")
+        ckpt = torch.load("/projects/leelab/mingyulu/data_att/results/cifar/retrain/models/full/steps_00125000.pt")
+
+        # ckpt = torch.load("results/cifar/retrain/models/full/steps_00078200.pt")
 
         model_full.load_state_dict(ckpt["model"])
         model_full.eval()
 
         transform = transforms.Compose(
             [
-                transforms.ToTensor(),
+                transforms.Resize(299),
+                transforms.CenterCrop(299),
                 transforms.RandomHorizontalFlip(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                transforms.ToTensor(),
+                transforms.Normalize(mean=config['mean'], std=config['std'])
             ]
         )
 
+        mean = torch.tensor(config['mean']).view( -1, 1, 1).to(device)
+        std = torch.tensor(config['std']).view( -1, 1, 1).to(device)
+
         trainset = torchvision.datasets.CIFAR10(
-            root='/data2/mingyulu/cfair_10', 
+            root=f'/projects/leelab/mingyulu/data_att/{args.dataset}',
             train=True,
             download=True, 
             transform=transform,
         )
-
-        inception_model = inception_v3(
-            weights='DEFAULT',
-            transform_input=False,
-            aux_logits=True
-        ).to(device)
-        
-        inception_model.to(device)
-        inception_model.eval()
-
         batch_size = 128  
 
         trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True) 
+        
+        block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[2048]
 
-        # Calculate real image features
-        real_features = get_features(trainloader, inception_model, n_samples=args.n_samples)
+        inception = InceptionV3([block_idx]).to(device)
 
-
+        real_features = get_features(
+            trainloader, 
+            inception, 
+            args.n_samples,
+            device
+        )
+        
         fake_features = []
+        fake_features_norm = []
 
         n_batches = args.n_samples // batch_size
-        
+
         with torch.no_grad(): 
             for _ in range(n_batches):
-                x_t = torch.randn((batch_size, config["in_channels"], config["image_size"], config["image_size"])).to(device)
-                samples = model_full._sampling(x_t, batch_size, clipped_reverse_diffusion=not args.no_clip, device=device)
+
+                samples = model_full.sampling(batch_size, clipped_reverse_diffusion=not args.no_clip, device=device)
+
+                normalized_samples = torch.stack([TF.resize((sample - mean)/std, (299, 299), antialias=True) for sample in samples])
+
                 samples = torch.stack([TF.resize(sample, (299, 299), antialias=True) for sample in samples])
-            
-                fake_feat = inception_model(samples).cpu().numpy()
+
+                fake_feat = inception(samples)[0]
+                fake_feat = fake_feat.squeeze(3).squeeze(2).cpu().numpy()
                 fake_features.append(fake_feat)
+
+                fake_feat_norm = inception(normalized_samples)[0]
+                fake_feat_norm = fake_feat_norm.squeeze(3).squeeze(2).cpu().numpy()
+
+                fake_features_norm.append(fake_feat_norm)
 
                 if torch.cuda.is_available():
                     torch.cuda.empty_cache()
 
         fake_features = np.concatenate(fake_features, axis=0)
+        fake_features_norm = np.concatenate(fake_features_norm, axis=0)
 
         # Calculate FID
         fid_value = calculate_fid(real_features, fake_features)
         print('FID:', fid_value)
+
+        fid_value = calculate_fid(real_features, fake_features_norm)
+        print('FID (norm):', fid_value)
 
 
 if __name__=="__main__":

diffusion/diffusions.py
@@ -3,6 +3,7 @@ import torch
 import math
 
 from diffusion.models import UNet
+from diffusion.unet_old import Unet as Old_Unet
 from tqdm import tqdm
 
 
@@ -14,7 +15,11 @@ class DDPM(nn.Module):
             channel_mult=[1, 2, 3, 4],
             image_size:int=32,
             in_channels:int=3,
-            out_channels:int=3
+            out_channels:int=3,
+            attn: bool=True,
+            attn_layer=[2],
+            num_res_blocks:int=2,
+            dropout: int= 0.15,
         ):
 
         super().__init__()
@@ -23,7 +28,7 @@ class DDPM(nn.Module):
         self.image_size=image_size
         self.in_channels = in_channels
 
-        betas=self._cosine_variance_schedule(timesteps)
+        betas= self._cosine_variance_schedule(timesteps)
 
         alphas=1.-betas
         alphas_cumprod=torch.cumprod(alphas,dim=-1)
@@ -34,16 +39,26 @@ class DDPM(nn.Module):
         self.register_buffer("sqrt_alphas_cumprod",torch.sqrt(alphas_cumprod))
         self.register_buffer("sqrt_one_minus_alphas_cumprod",torch.sqrt(1.-alphas_cumprod))
 
-        self.model = UNet(
-            T=timesteps,
-            ch=base_dim,
-            ch_mult=channel_mult,
-            attn=[2],
-            num_res_blocks=2,
-            dropout=0.15,
-            input_ch_dim = in_channels,
-            output_ch_dim = out_channels
-        )
+        if not attn:
+            self.model = Old_Unet(
+                timesteps=1000,
+                time_embedding_dim=256,
+                in_channels=1,
+                out_channels=1,
+                base_dim=64,
+                dim_mults=[2,4]
+            )
+        else:
+            self.model = UNet(
+                T=timesteps,
+                ch=base_dim,
+                ch_mult=channel_mult,
+                attn=attn_layer,
+                num_res_blocks=num_res_blocks,
+                dropout=dropout,
+                input_ch_dim = in_channels,
+                output_ch_dim = out_channels
+            )
 
 
     def forward(

train_diffusion.py
@@ -6,7 +6,6 @@ import torch
 import torch.nn as nn
 
 from torchvision.utils import save_image
-from torch.utils.data import DataLoader
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import OneCycleLR
 
@@ -52,7 +51,11 @@ def main(args):
             channel_mult=config['channel_mult'],
             image_size=config['image_size'],
             in_channels=config['in_channels'],
-            out_channels=config['out_channels']
+            out_channels=config['out_channels'],
+            attn=config['attn'],
+            attn_layer=config['attn_layer'],
+            num_res_blocks=config['num_res_blocks'],
+            dropout=config['dropout']
         ).to(device)
 
         mean = torch.tensor(config['mean']).view(1, -1, 1, 1).to(device)
@@ -63,7 +66,6 @@ def main(args):
         train_dataloader, _ = create_dataloaders(
             dataset_name=config["dataset"],
             batch_size=config["batch_size"],
-            image_size=config['image_size'],
             excluded_class=excluded_class,
             unlearning=False
         )
@@ -141,7 +143,7 @@ def main(args):
             ## Generate samples and calculate fid score for non-mnist dataset every 15 epochs
             excluded_class = "full" if excluded_class is None else excluded_class
 
-            if  (epoch+1) % 15 == 0 or (epoch+1) % config['epochs'] == 0:
+            if  (epoch+1) % 15 == 0 or (epoch+1) % config['epochs'] == 0 or global_steps == config['epochs']*len(train_dataloader):
                 
                 model_ema.eval()
                 samples = model_ema.module.sampling(
@@ -155,8 +157,8 @@ def main(args):
                     fid_scores.append(fid_value)
                     print(f"FID score after {global_steps} steps: {fid_value}")
                 
-                os.makedirs(f"results/{config['dataset']}/retrain/samples/{excluded_class}", exist_ok=True)
-                save_image((samples*std + mean), f"results/{config['dataset']}/retrain/samples/{excluded_class}/steps_{global_steps:0>8}.png", nrow=int(math.sqrt(args.n_samples)))
+                os.makedirs(f"results/{args.dataset}/retrain/samples/{excluded_class}", exist_ok=True)
+                save_image(samples, f"results/{args.dataset}/retrain/samples/{excluded_class}/steps_{global_steps:0>8}.png", nrow=int(math.sqrt(args.n_samples)))
 
 
             ## Checkpoints for training
@@ -164,12 +166,13 @@ def main(args):
             if  (epoch+1) % (config['epochs'] //2) == 0 or (epoch+1) % config['epochs'] == 0:
 
                 print(f"Checkpoint saved at step {global_steps}")
-                os.makedirs(f"/data2/mingyulu/data_att/results/{config['dataset']}/retrain/models/{excluded_class}", exist_ok=True)
+
+                os.makedirs(f"/projects/leelab/mingyulu/data_att/results/{args.dataset}/retrain/models/{excluded_class}", exist_ok=True)
                 ckpt = {
                     "model": model.state_dict(),
                     "model_ema": model_ema.state_dict()
                 }
-                torch.save(ckpt, f"/data2/mingyulu/data_att/results/{config['dataset']}/retrain/models/{excluded_class}/steps_{global_steps:0>8}.pt")
+                torch.save(ckpt, f"/projects/leelab/mingyulu/data_att/results/{args.dataset}/retrain/models/{excluded_class}/steps_{global_steps:0>8}.pt")
 
 
         ckpt = {
@@ -177,10 +180,11 @@ def main(args):
             "model_ema": model_ema.state_dict()
         }
 
-        torch.save(ckpt, f"/data2/mingyulu/data_att/results/{config['dataset']}/retrain/models/{excluded_class}/steps_{global_steps:0>8}.pt")
+
+        torch.save(ckpt, f"/projects/leelab/mingyulu/data_att/results/{args.dataset}/retrain/models/{excluded_class}/steps_{global_steps:0>8}.pt")
 
         if config['dataset'] != "mnist":
-            np.save(f"/data2/mingyulu/data_att/results/{config['dataset']}/retrain/models/{excluded_class}/steps_{global_steps:0>8}.npy",  np.array(fid_scores))
+            np.save(f"/projects/leelab/mingyulu/data_att/results/{args.dataset}/retrain/models/{excluded_class}/steps_{global_steps:0>8}.npy",  np.array(fid_scores))
 
 
 if __name__=="__main__":

train_unlearning.py
@@ -71,25 +71,17 @@ def main(args):
     else:
         raise ValueError(f"Unknown dataset {config['dataset']}, choose 'cifar' or 'mnist'.")
 
-    model = DDPM(
-        timesteps=config['timesteps'],
-        base_dim=config['base_dim'],
-        channel_mult=config['channel_mult'],
-        image_size=config['image_size'],
-        in_channels=config['in_channels'],
-        out_channels=config['out_channels']
-    ).to(device)
-
-    mean = torch.tensor(config['mean']).view(1, -1, 1, 1).to(device)
-    std = torch.tensor(config['std']).view(1, -1, 1, 1).to(device)
-
     model_frozen = DDPM(
         timesteps=config['timesteps'],
         base_dim=config['base_dim'],
         channel_mult=config['channel_mult'],
         image_size=config['image_size'],
         in_channels=config['in_channels'],
-        out_channels=config['out_channels']
+        out_channels=config['out_channels'],
+        attn=config['attn'],
+        attn_layer=config['attn_layer'],        
+        num_res_blocks=config['num_res_blocks'],
+        dropout=config['dropout'],
     ).to(device)
 
     ## loss params
@@ -103,9 +95,10 @@ def main(args):
     alpha = 1.0 - args.model_ema_decay
     alpha = min(1.0, alpha * adjust)
 
-    #load checkpoint - old_ckpt=torch.load("/projects/leelab2/mingyulu/unlearning/results/full/models/steps_00042300.pt")
 
-    ckpt = load_checkpoint(f"results/{args.dataset}/retrain/models/full/")
+    ckpt=torch.load(config['trained_model'])
+
+    # ckpt = load_checkpoint(f"results/{args.dataset}/retrain/models/full/")
 
     model_frozen.load_state_dict(ckpt["model"])
     model_frozen.eval()
@@ -118,13 +111,12 @@ def main(args):
 
     for excluded_class in range(10):
 
-        path = f"/data2/mingyulu/data_att/results/{args.dataset}/unlearn_remaining_ablated/"
+        path = f"/projects/leelab/mingyulu/data_att/results/{args.dataset}/unlearning/"
         params = f"/{excluded_class}/epochs={args.epochs}_datasets={args.keep_digits}_lr={args.lr}_loss={args.loss_type}:alpha1={alpha1}_alpha2={alpha2}_weight_reg={args.weight_reg}"
 
         train_dataloader, ablated_dataloader = create_dataloaders(
             dataset_name=args.dataset,
-            batch_size=args.batch_size,
-            image_size=config['image_size'],
+            batch_size=config['batch_size'],
             excluded_class=excluded_class,
             unlearning=True
         )
@@ -137,13 +129,20 @@ def main(args):
             channel_mult=config['channel_mult'],
             image_size=config['image_size'],
             in_channels=config['in_channels'],
-            out_channels=config['out_channels']
+            out_channels=config['out_channels'],
+            attn=config['attn'],
+            attn_layer=config['attn_layer'],
+            num_res_blocks=config['num_res_blocks'],
+            dropout=config['dropout'],
         ).to(device)
 
 
         model_ema = ExponentialMovingAverage(model, device=device, decay=1.0 - alpha)
 
-        ckpt = load_checkpoint(f"results/{args.dataset}/retrain/models/full/")
+
+        ckpt=torch.load(config['trained_model'])
+
+        # ckpt = load_checkpoint(f"results/{args.dataset}/retrain/models/full/")
 
         model.load_state_dict(ckpt["model"])
         model_ema.load_state_dict(ckpt["model_ema"])
@@ -215,6 +214,7 @@ def main(args):
 
                 if global_steps%args.model_ema_steps==0:
                     model_ema.update_parameters(model)
+
                 global_steps+=1
 
                 if j % args.log_freq == 0:
@@ -225,19 +225,23 @@ def main(args):
                 "model_ema": model_ema.state_dict()
             }
 
-            if (epoch+1)%10 ==0 or (epoch+1)% args.epochs==0:
+            if (epoch+1)%10 ==0 or (epoch+1)% args.epochs==0 or global_steps == config['epochs']*len(train_dataloader):
 
                 model_ema.eval()
 
-                os.makedirs(path, exist_ok=True)
-                os.makedirs(path + "samples" + params, exist_ok=True)
+                os.makedirs(f"results/{args.dataset}/unlearning/samples", exist_ok=True)
+                os.makedirs(f"results/{args.dataset}/unlearning/samples" + params, exist_ok=True)
 
-                samples = model_ema.module.sampling(args.n_samples, clipped_reverse_diffusion=not args.no_clip, device=device)
-                save_image(samples, path + "samples" + params + f"/steps_{global_steps:0>8}.png", nrow=int(math.sqrt(args.n_samples)))
+                samples = model_ema.module.sampling(
+                    args.n_samples, 
+                    clipped_reverse_diffusion=not args.no_clip, 
+                    device=device
+                )
+                save_image(samples,f"results/{args.dataset}/unlearning/samples" + params + f"/steps_{global_steps:0>8}.png", nrow=int(math.sqrt(args.n_samples)))
 
         os.makedirs(path + "models" + params, exist_ok=True)
-        torch.save(ckpt,path + "models" + params +  f"/steps_{global_steps:0>8}.pt")
-
+        torch.save(ckpt, path + "models" + params +  f"/steps_{global_steps:0>8}.pt")
+   
 if __name__=="__main__":
     args=parse_args()
     main(args)

utils.py
@@ -1,3 +1,5 @@
+import glob
+import os
 import torch
 import torch.nn.functional as F
 import numpy as np
@@ -13,6 +15,13 @@ from torchvision.models import inception_v3
 from scipy.linalg import sqrtm
 from CLIP.clip import clip
 from PIL import Image
+
+
+
+# Load CLIP model and transformation outside of the function for efficiency
+device = "cuda" if torch.cuda.is_available() else "cpu"
+clip_model, clip_transform = clip.load("ViT-B/32", device=device)
+
 
 class ExponentialMovingAverage(torch.optim.swa_utils.AveragedModel):
     """Maintains moving averages of model parameters using an exponential decay.
@@ -30,7 +39,6 @@ class ExponentialMovingAverage(torch.optim.swa_utils.AveragedModel):
 def create_dataloaders(
     dataset_name: str,
     batch_size: int,
-    image_size: int = 28,
     num_workers: int = 4,
     excluded_class: int = None,
     unlearning: bool = False
@@ -58,16 +66,15 @@ def create_dataloaders(
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
         DatasetClass = CIFAR10
-        root_dir = '/data2/mingyulu/data_att/cifar'
+        root_dir = '/projects/leelab/mingyulu/data_att/cifar'
     
     elif dataset_name == 'mnist':
         preprocess = transforms.Compose([
-            transforms.Resize(image_size),
             transforms.ToTensor(),
             transforms.Normalize([0.5], [0.5])
         ])
         DatasetClass = MNIST
-        root_dir = '/data2/mingyulu/data_att/mnist'
+        root_dir = '/projects/leelab/mingyulu/data_att/mnist'
     else:
         raise ValueError(f"Unknown dataset {dataset_name}, choose 'cifar' or 'mnist'.")
 
@@ -105,9 +112,56 @@ def create_dataloaders(
     return train_loader, test_loader
 
 
-def calculate_fid(images1, images2, device):
+
+def find_max_step_file(path_pattern):
+    """
+    Obtain max steps pt in a folder.
+
+    """
+    files = glob.glob(path_pattern)
+    max_step_file = max(files, key=lambda x: int(os.path.basename(x).split('_')[1].split('.')[0]))
+    return max_step_file
+
+
+def get_features(
+        dataloader, 
+        model, 
+        n_samples, 
+        device
+    ) -> np.ndarray:
+    
+    """
+    Feature extraction for Inception V3
     """
 
+    model.eval()
+    features = []
+
+    processed_samples = 0
+    
+    for images, _ in dataloader:
+        if processed_samples >= n_samples:
+            break
+        
+        with torch.no_grad():
+            # Apply preprocessing to each image in the batch
+
+            images = images.to(device)
+
+            batch_features = model(images)[0]
+            batch_features = batch_features.squeeze(3).squeeze(2).cpu().numpy()
+
+            features.append(batch_features)
+            processed_samples += images.size(0)
+
+    features = np.concatenate(features, axis=0)
+
+    # Trim the features if necessary
+    return features[:n_samples]
+
+
+def calculate_fid(images1, images2, device):
+    """
     Create a mnist dataset with/without filtered labels
 
     Args:
@@ -118,7 +172,7 @@ def calculate_fid(images1, images2, device):
     """
 
     model = inception_v3(
-        weights='DEFAULT',
+        weights='pretrained',
         transform_input=False,
         aux_logits=True
     ).to(device)
@@ -134,8 +188,8 @@ def calculate_fid(images1, images2, device):
         act1 = model(images1)
         act2 = model(images2)
 
-    act1 = act1.cpu().numpy()
-    act2 = act2.cpu().numpy()
+    act1 = act1.detach().cpu().numpy()
+    act2 = act2.detach().cpu().numpy()
 
     mu1, sigma1 = act1.mean(axis=0), np.cov(act1, rowvar=False)
     mu2, sigma2 = act2.mean(axis=0), np.cov(act2, rowvar=False)
@@ -152,10 +206,6 @@ def calculate_fid(images1, images2, device):
     fid = ss_diff + np.trace(sigma1 + sigma2 - 2.0 * cov_mean)
 
     return fid
-
-# Load CLIP model and transformation outside of the function for efficiency
-device = "cuda" if torch.cuda.is_available() else "cpu"
-clip_model, clip_transform = clip.load("ViT-B/32", device=device)
 
 def preprocess_clip_mnist(batch_images):
     """