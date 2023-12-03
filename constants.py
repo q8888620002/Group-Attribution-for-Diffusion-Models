f6558ba51af4ea7ae6fb0231b0772585acc41da7
Mingyu Lu <mingyulu@l2lambda.cs.washington.edu>
2023-12-03 12:10:33 -0800

    .gitignore is now working

--- constants.py
+++
@@ -1,5 +0,0 @@
-"""Global constant variables for the project."""
-
-DATASET_DIR = "/projects/leelab/mingyulu/data_att"
-OUTDIR = "/projects/leelab/mingyulu/data_att/results"
-MAX_NUM_SAMPLE_IMAGES_TO_SAVE = 500
