23d87f81fc27bb808bc64ef8b2214babe8da189f
Chris Lin <chris522229197@gmail.com>
2024-01-16 14:20:50 -0800

    Revamp generate_samples.py

--- generate_samples.py
+++ generate_samples.py
@@ -1,13 +1,11 @@
 """Generate samples for a given diffusion model."""
 import argparse
-import math
 import os
 
 import diffusers
 import numpy as np
 import torch
 from diffusers import (
-    DDIMPipeline,
     DDIMScheduler,
     DDPMPipeline,
     DDPMScheduler,
@@ -18,11 +16,11 @@ from diffusers import (
 from diffusers.training_utils import EMAModel
 from lightning.pytorch import seed_everything
 from torchvision.utils import save_image
+from tqdm import tqdm
 
 import constants
 from ddpm_config import DDPMConfig
-from diffusion.models import CNN
-from utils import ImagenetteCaptioner, LabelTokenizer, create_dataset, get_max_steps
+from utils import ImagenetteCaptioner, create_dataset, get_max_steps
 
 
 def parse_args():
@@ -34,10 +32,13 @@ def parse_args():
         type=str,
         help="dataset for training or unlearning",
         choices=["mnist", "cifar", "celeba", "imagenette"],
-        default="mnist",
+        default="cifar",
     )
     parser.add_argument(
-        "--batch_size", type=int, default=512, help="batch size for sample generation."
+        "--n_samples", type=int, default=100000, help="number of generated samples"
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=512, help="batch size for sample generation"
     )
     parser.add_argument(
         "--excluded_class",
@@ -73,28 +74,11 @@ def parse_args():
     parser.add_argument(
         "--seed",
         type=int,
-        help="random seed for model training or unlearning",
+        help="random seed for image sample generation",
         default=42,
     )
     parser.add_argument(
         "--outdir", type=str, help="output parent directory", default=constants.OUTDIR
-    )
-    parser.add_argument(
-        "--db", type=str, help="database file for storing results", default=None
-    )
-    parser.add_argument(
-        "--exp_name", type=str, help="experiment name in the database", default=None
-    )
-    parser.add_argument(
-        "--mixed_precision",
-        type=str,
-        default="no",
-        choices=["no", "fp16", "bf16"],
-        help=(
-            "Whether to use mixed precision. Choose"
-            "between fp16 and bf16 (bfloat16). Bf16 requires PyTorch >= 1.10."
-            "and an Nvidia Ampere GPU."
-        ),
     )
     parser.add_argument(
         "--num_inference_steps",
@@ -103,10 +87,28 @@ def parse_args():
         help="number of diffusion steps for generating images",
     )
     parser.add_argument(
-        "--num_train_steps",
-        type=int,
-        default=1000,
-        help="number of diffusion steps during training",
+        "--use_ema",
+        help="whether to use the EMA model",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--ema_inv_gamma",
+        type=float,
+        default=1.0,
+        help="inverse gamma value for EMA decay",
+    )
+    parser.add_argument(
+        "--ema_power",
+        type=float,
+        default=3 / 4,
+        help="power value for EMA decay",
+    )
+    parser.add_argument(
+        "--ema_max_decay",
+        type=float,
+        default=0.9999,
+        help="maximum decay magnitude EMA",
     )
     parser.add_argument(
         "--device", type=str, help="device of training", default="cuda:0"
@@ -116,7 +118,7 @@ def parse_args():
 
 
 def main(args):
-    """Function to calculate model behavior"""
+    """Main function to generate samples from a diffusion model."""
     seed_everything(args.seed)
     device = args.device
 
@@ -126,11 +128,6 @@ def main(args):
         config = {**DDPMConfig.celeba_config}
     elif args.dataset == "mnist":
         config = {**DDPMConfig.mnist_config}
-        classifier = CNN().to(device)
-        classifier.load_state_dict(
-            torch.load("eval/models/epochs=10_cnn_weights.pt", map_location=device)
-        )
-        classifier.eval()
     elif args.dataset == "imagenette":
         config = {**DDPMConfig.imagenette_config}
     else:
@@ -159,61 +156,55 @@ def main(args):
         removal_dir,
     )
     sample_outdir = os.path.join(
-        args.outdir, args.dataset, args.method, "samples", removal_dir
+        args.outdir,
+        args.dataset,
+        args.method,
+        "generated_samples",
+        removal_dir,
     )
+    os.makedirs(sample_outdir, exist_ok=True)
 
-    # Load pre-trained model.
-
-    pretrained_steps = get_max_steps(model_loaddir)
-
-    if pretrained_steps is not None:
-        ckpt_path = os.path.join(model_loaddir, f"ckpt_steps_{pretrained_steps:0>8}.pt")
+    # Load the trained U-Net model or U-Net EMA.
+    trained_steps = get_max_steps(model_loaddir)
+    if trained_steps is not None:
+        ckpt_path = os.path.join(model_loaddir, f"ckpt_steps_{trained_steps:0>8}.pt")
         ckpt = torch.load(ckpt_path, map_location="cpu")
         model = model_cls(**config["unet_config"])
         model.load_state_dict(ckpt["unet"])
+        model_str = "U-Net"
 
-        ema_model = EMAModel(
-            model.parameters(),
-            decay=args.ema_max_decay,
-            use_ema_warmup=False,
-            inv_gamma=args.ema_inv_gamma,
-            power=args.ema_power,
-            model_cls=model_cls,
-            model_config=model.config,
-        )
+        if args.use_ema:
+            ema_model = EMAModel(
+                model.parameters(),
+                decay=args.ema_max_decay,
+                use_ema_warmup=False,
+                inv_gamma=args.ema_inv_gamma,
+                power=args.ema_power,
+                model_cls=model_cls,
+                model_config=model.config,
+            )
+            ema_model.load_state_dict(ckpt["unet_ema"])
+            ema_model.copy_to(model.parameters())
+            model_str = "EMA"
 
-        print(f"Pre-trained model loaded from {args.load}")
-        print(f"\tU-Net loaded from {ckpt_path}")
-        print("\tEMA started from the loaded U-Net")
+        print(f"Trained model loaded from {model_loaddir}")
+        print(f"\t{model_str} loaded from {ckpt_path}")
     else:
-        raise ValueError(f"No pre-trained checkpoints found at {model_loaddir}")
+        raise ValueError(f"No trained checkpoints found at {model_loaddir}")
 
-    ema_model.to(device)
-
+    # Get the diffusion model pipeline for inference.
     if args.dataset == "imagenette":
         # The pipeline is of class LDMTextToImagePipeline.
         train_dataset = create_dataset(dataset_name=args.dataset, train=True)
-        pipeline = DiffusionPipeline.from_pretrained("CompVis/ldm-text2im-large-256")
-        pipeline.unet = model
-
-        vqvae = pipeline.vqvae
-        text_encoder = pipeline.bert
-        tokenizer = pipeline.tokenizer
         captioner = ImagenetteCaptioner(train_dataset)
-        label_tokenizer = LabelTokenizer(captioner=captioner, tokenizer=tokenizer)
 
-        vqvae.requires_grad_(False)
-        text_encoder.requires_grad_(False)
-
-        vqvae = vqvae.to(device)
-        text_encoder = text_encoder.to(device)
+        pipeline = DiffusionPipeline.from_pretrained(
+            "CompVis/ldm-text2im-large-256"
+        ).to(device)
+        pipeline.unet = model.to(device)
     elif args.dataset == "celeba":
         model_id = "CompVis/ldm-celebahq-256"
         vqvae = VQModel.from_pretrained(model_id, subfolder="vqvae")
-
-        for param in vqvae.parameters():
-            param.requires_grad = False
-
         pipeline = LDMPipeline(
             unet=model,
             vqvae=vqvae,
@@ -224,62 +215,61 @@ def main(args):
             unet=model, scheduler=DDPMScheduler(**config["scheduler_config"])
         ).to(device)
 
-    pipeline_scheduler = pipeline.scheduler
+    # Generate images.
+    batch_size_list = [args.batch_size] * (args.n_samples // args.batch_size)
+    remaining_sample_size = args.n_samples % args.batch_size
+    if remaining_sample_size > 0:
+        batch_size_list.append(remaining_sample_size)
 
-    with torch.no_grad():
-        if args.dataset == "imagenette":
-            samples = []
-            n_samples_per_cls = math.ceil(config["n_samples"] / captioner.num_classes)
-            classes = [idx for idx in range(captioner.num_classes)]
-            for _ in range(n_samples_per_cls):
-                samples.append(
-                    pipeline(
-                        prompt=captioner(classes),
-                        num_inference_steps=50,
+    if args.dataset != "imagenette":
+        # For unconditional diffusion models.
+        generated_images = []
+        with torch.no_grad():
+            for batch_size in tqdm(batch_size_list):
+                images = pipeline(
+                    batch_size=batch_size,
+                    num_inference_steps=args.num_inference_steps,
+                    output_type="numpy",
+                ).images
+                generated_images.append(images)
+        generated_images = np.concatenate(generated_images, axis=0)
+
+        for i, image in enumerate(generated_images):
+            save_image(
+                torch.from_numpy(image).permute([2, 0, 1]),
+                os.path.join(sample_outdir, f"seed={args.seed}_sample_{i}.png"),
+            )
+        print(f"Generated samples saved to {sample_outdir}")
+    else:
+        # Conditoinal generation for each class in Imagenette.
+        for class_idx in range(captioner.num_classes):
+            generated_images = []
+            with torch.no_grad():
+                for batch_size in tqdm(batch_size_list):
+                    images = pipeline(
+                        prompt=captioner([class_idx] * batch_size),
+                        num_inference_steps=args.num_inference_steps,
                         eta=0.3,
                         guidance_scale=6,
                         output_type="numpy",
                     ).images
+                    generated_images.append(images)
+            generated_images = np.concatenate(generated_images, axis=0)
+
+            synset = captioner.label_to_synset[class_idx]
+            synset_sample_outdir = os.path.join(sample_outdir, synset)
+            os.makedirs(synset_sample_outdir, exist_ok=True)
+            for i, image in enumerate(generated_images):
+                save_image(
+                    torch.from_numpy(image).permute([2, 0, 1]),
+                    os.path.join(
+                        synset_sample_outdir, f"seed={args.seed}_sample_{i}.png"
+                    ),
                 )
-            samples = np.concatenate(samples)
-        elif args.dataset == "celeba":
-            pipeline = LDMPipeline(
-                unet=model,
-                vqvae=vqvae,
-                scheduler=pipeline_scheduler,
-            ).to(device)
-        else:
-            pipeline = DDIMPipeline(
-                unet=model,
-                scheduler=DDIMScheduler(num_train_timesteps=args.num_train_steps),
-            )
-
-        generated_imgs = []
-        n_batches = args.n_samples // args.batch_size
-
-        # TODO add text-to-image generation
-
-        for index in range(n_batches):
-            images = pipeline(
-                batch_size=args.batch_size,
-                num_inference_steps=args.num_inference_steps,
-                output_type="numpy",
-            ).images
-
-            generated_imgs.append(images)
-            print(f"generating samples {index}/{n_batches}")
-
-        generated_imgs = np.concatenate(generated_imgs, axis=0)
-
-    for i, images in enumerate(generated_imgs):
-        save_image(
-            torch.from_numpy(images).permute([2, 0, 1]),
-            os.path.join(sample_outdir, f"sample_{i:04d}.png"),
-        )
-
-    print(f"Images saved to {sample_outdir}")
+            print(f"Generated sampels saved to {synset_sample_outdir}")
 
 
 if __name__ == "__main__":
     args = parse_args()
     main(args)
+    print("Sample generation completed!")
