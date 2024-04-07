dfccc91cf24c0fd88771b5ee3133c85f6283e2d0
Chris Lin <chris522229197@gmail.com>
2024-04-06 19:09:27 -0700

    Add setup for pruned fine-tuning models

--- text_to_image/experiments/setup_model_behavior_commands.py
+++ text_to_image/experiments/setup_model_behavior_commands.py
@@ -34,7 +34,7 @@ def parse_args():
     parser.add_argument(
         "--method",
         type=str,
-        choices=["retrain", "pretrained"],
+        choices=["retrain", "pretrained", "pruned_ft"],
         default="retrain",
         help="training or unlearning method",
     )
@@ -52,10 +52,16 @@ def parse_args():
         default=500,
     )
     parser.add_argument(
-        "--num_subsets_per_job",
+        "--num_executions_per_job",
         type=int,
-        help="number of removal subsets to run for each SLURM job",
+        help="number of script executions to run for each SLURM job",
         default=10,
+    )
+    parser.add_argument(
+        "--num_images",
+        type=int,
+        default=50,
+        help="number of images to generate for computing model behaviors",
     )
     args = parser.parse_args()
     return args
@@ -91,6 +97,8 @@ def main(args):
     if args.dataset == "artbench_post_impressionism":
         config = TextToImageModelBehaviorConfig.artbench_post_impressionism_config
         config["seed"] = args.model_behavior_seed
+        config["num_images"] = args.num_images
+        num_update_steps_per_epoch = 79
     else:
         raise ValueError
 
@@ -116,6 +124,7 @@ def main(args):
         with open(command_file, "w") as handle:
             handle.write(command + "\n")
             num_jobs += 1
+
     elif args.method == "retrain" and args.removal_method == "full":
         command = "python text_to_image/compute_model_behaviors.py"
         for key, val in config.items():
@@ -133,8 +142,59 @@ def main(args):
         with open(command_file, "w") as handle:
             handle.write(command + "\n")
             num_jobs += 1
+
+    elif args.method == "pruned_ft" and args.removal_method == "full":
+        ratio_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        lora_steps_list = [
+            num_update_steps_per_epoch * num_epochs
+            for num_epochs in [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]
+        ]
+        assert (
+            len(ratio_list) * len(lora_steps_list)
+        ) % args.num_executions_per_job == 0
+        ckpt_dir = os.path.join(ckpt_dir, "pruned_ft")
+        os.makedirs(ckpt_dir, exist_ok=True)
+
+        num_executions = 0
+        with open(command_file, "w") as handle:
+            command = ""
+            for ratio in ratio_list:
+                for lora_steps in lora_steps_list:
+                    command += "python text_to_image/compute_model_behaviors.py"
+                    for key, val in config.items():
+                        command += " " + format_config_arg(key, val)
+                    ckpt_path = os.path.join(
+                        ckpt_dir, f"ratio_{ratio}_lora_steps_{lora_steps}.pt"
+                    )
+                    db = os.path.join(db_dir, "pruned_ft.jsonl")
+                    lora_dir = os.path.join(
+                        OUTDIR,
+                        f"seed{args.opt_seed}",
+                        args.dataset,
+                        f"pruned_ft_ratio={ratio}",
+                        "models",
+                        "full",
+                    )
+                    command += " --ckpt_path={}".format(ckpt_path)
+                    command += " --db={}".format(db)
+                    command += " --exp_name={}".format(
+                        os.path.join(
+                            exp_name, f"ratio={ratio}", f"lora_steps={lora_steps}"
+                        )
+                    )
+                    command += " --lora_dir={}".format(lora_dir)
+                    command += " --lora_steps={}".format(lora_steps)
+                    num_executions += 1
+
+                    if num_executions % args.num_executions_per_job == 0:
+                        handle.write(command + "\n")
+                        command = ""
+                        num_jobs += 1
+                    else:
+                        command += " ; "
+
     elif args.removal_method is not None:
-        assert args.num_removal_subsets % args.num_subsets_per_job == 0
+        assert args.num_removal_subsets % args.num_executions_per_job == 0
         removal_dist = args.removal_method.split("_")[-1]
         ckpt_dir = os.path.join(ckpt_dir, args.removal_method)
         os.makedirs(ckpt_dir, exist_ok=True)
@@ -165,7 +225,7 @@ def main(args):
                 )
                 command += " --lora_dir={}".format(lora_dir)
 
-                if (removal_seed + 1) % args.num_subsets_per_job == 0:
+                if (removal_seed + 1) % args.num_executions_per_job == 0:
                     handle.write(command + "\n")
                     command = ""
                     num_jobs += 1
