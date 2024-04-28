40abda1e1b7dedf7333f93310eb0a91b978e7037
Chris Lin <chris522229197@gmail.com>
2024-04-28 09:22:44 -0700

    Check for completed pruning jobs

--- text_to_image/experiments/setup_prune_commands.py
+++ text_to_image/experiments/setup_prune_commands.py
@@ -69,37 +69,53 @@ def main(args):
         for i in range(1, 10):
             pruning_ratio = i / 10
             for learning_rate in [1e-5, 3e-5, 1e-4, 3e-4, 1e-3, 3e-3]:
-                command = "python text_to_image/prune_lora.py"
-                command += " --lora_dir={}".format(trained_lora_dir)
-                command += " --pruning_ratio={}".format(pruning_ratio)
-                command += " ; "
-                command += "accelerate launch"
-                command += " --gpu_ids=0"
-                command += " --mixed_precision={}".format("fp16")
-                command += " text_to_image/train_text_to_image_lora.py"
-                for key, val in training_config.items():
-                    if key not in ["learning_rate"]:
-                        command += " " + format_config_arg(key, val)
-                command += " --learning_rate={}".format(learning_rate)
-                command += " --checkpoint_attn_procs"
-                command += " --pruning_ratio={}".format(pruning_ratio)
+                model_outdir = os.path.join(
+                    training_config["output_dir"],
+                    args.dataset,
+                    f"pruned_ft_ratio={pruning_ratio}_lr={learning_rate}",
+                    "models",
+                    "full",
+                )
+                weight_file = os.path.join(
+                    model_outdir, "pytorch_lora_weights.safetensors"
+                )
+                if not os.path.exists(weight_file):
+                    command = "python text_to_image/prune_lora.py"
+                    command += " --lora_dir={}".format(trained_lora_dir)
+                    command += " --pruning_ratio={}".format(pruning_ratio)
+                    command += " ; "
+                    command += "accelerate launch"
+                    command += " --gpu_ids=0"
+                    command += " --mixed_precision={}".format("fp16")
+                    command += " text_to_image/train_text_to_image_lora.py"
+                    for key, val in training_config.items():
+                        if key not in ["learning_rate"]:
+                            command += " " + format_config_arg(key, val)
+                    command += " --learning_rate={}".format(learning_rate)
+                    command += " --checkpoint_attn_procs"
+                    command += " --pruning_ratio={}".format(pruning_ratio)
 
-                handle.write(command + "\n")
-                num_jobs += 1
-    print(f"Commands saved to {command_file}")
+                    handle.write(command + "\n")
+                    num_jobs += 1
+    if num_jobs > 0:
+        print(f"Commands saved to {command_file}")
 
-    # Update the SLURM job submission file.
-    job_file = os.path.join(os.getcwd(), "text_to_image", "experiments", "prune.job")
-    array = f"1-{num_jobs}" if num_jobs > 1 else "1"
-    job_name = "prune-" + exp_name.replace("/", "-")
-    output = os.path.join(logdir, "run-%A-%a.out")
-    update_job_file(
-        job_file=job_file,
-        job_name=job_name,
-        output=output,
-        array=array,
-        command_file=command_file,
-    )
+        # Update the SLURM job submission file.
+        job_file = os.path.join(
+            os.getcwd(), "text_to_image", "experiments", "prune.job"
+        )
+        array = f"1-{num_jobs}" if num_jobs > 1 else "1"
+        job_name = "prune-" + exp_name.replace("/", "-")
+        output = os.path.join(logdir, "run-%A-%a.out")
+        update_job_file(
+            job_file=job_file,
+            job_name=job_name,
+            output=output,
+            array=array,
+            command_file=command_file,
+        )
+    else:
+        print("All pruning jobs are completed!")
 
 
 if __name__ == "__main__":
